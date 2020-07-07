cdca0a43ea278fc12c9ebd69f20c8b0aa681e067
Maximilian Jugl <mjugl@hs-mittweida.de>
2020-07-07 21:40:44 +0200

    Uncommented line that was left out for debugging purposes

--- setup.py
+++ setup.py
@@ -135,7 +135,7 @@ def apply_patches():
         try:
             # Additionally, remove OpenCV module directory.
             logger.debug("Deleting OpenCV module directory")
-            #shutil.rmtree(OPENCV_MODULE_DIR)
+            shutil.rmtree(OPENCV_MODULE_DIR)
         except IOError:
             logger.error("Couldn't delete OpenCV module directory", exc_info=True)
 
